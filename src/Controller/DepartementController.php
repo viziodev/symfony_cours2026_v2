1819465e512749ec5828ec4adf3c23c53cd5fbcc
douvewane <douvewane85@gmail.com>
2025-10-16 04:40:39 +0000

    Pagination + Component

--- src/Controller/DepartementController.php
+++ src/Controller/DepartementController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Repository\DepartementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DepartementController extends AbstractController
 {
-
+   private const LIMIT=4;
     public function __construct(private readonly DepartementRepository $departementRepository)
     {
         
@@ -19,11 +20,19 @@ final class DepartementController extends AbstractController
         Creer un departement ==>POST(name)
      */
     #[Route('/departement/list', name: 'app_departement_list',methods:["GET","POST"])]
-    public function list(): Response
+    public function list(Request $request): Response
     {
-        $departements=$this->departementRepository->findAll();
+        $page=(int)$request->query->get("page",1);
+        $offset=($page-1)*self::LIMIT;
+        $filtre=[
+            "isArchived"=>false
+          ];
+        $departements=$this->departementRepository->findBy($filtre,[],self::LIMIT, $offset);
+        $nbrePage=ceil($this->departementRepository->count($filtre)/self::LIMIT);
         return $this->render('departement/list.html.twig', [
-            'departements' => $departements,
+             'departements' => $departements,
+             "pageEncours"=>$page,
+             "nbrePage"=>$nbrePage
         ]);
     }
 
--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Repository\DepartementRepository;
 use App\Repository\EmployeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class EmployeController extends AbstractController
 {
+       private const LIMIT=4;
 
     public function __construct(private readonly EmployeRepository $employeRepository,
                                 private readonly DepartementRepository $departementRepository)
@@ -26,9 +28,11 @@ final class EmployeController extends AbstractController
                       http://127.0.0.1:8000/employe/list/1
      */
     #[Route('/employe/list/{idDept?}', name: 'app_employe_list')]
-    public function list($idDept): Response
+    public function list($idDept,Request $request): Response
     { 
           $departement=null;
+          $page=(int)$request->query->get("page",1);
+          $offset=($page-1)*self::LIMIT;
           $filtre=[
             "isArchived"=>false
           ];
@@ -37,10 +41,13 @@ final class EmployeController extends AbstractController
               $departement=$this->departementRepository->find($idDept);
 
           }
-        $employes=$this->employeRepository->findBy($filtre);
+           $employes=$this->employeRepository->findBy($filtre,[],self::LIMIT, $offset);
+           $nbrePage=ceil($this->departementRepository->count($filtre)/self::LIMIT);
         return $this->render('employe/list.html.twig', [
             'employes' =>  $employes,
-            "departement"=>$departement
+            "departement"=>$departement,
+             "pageEncours"=>$page,
+             "nbrePage"=>$nbrePage
         ]);
     }
 
